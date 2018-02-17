2f29594c1a779e59d745d9c854dfdfdc622333b0
Gabriel Tu <gabriel8215@hotmail.com>
2018-02-17 14:57:30 -0500

    Push to fix kashifs broken files

--- app/src/main/java/greenhome/greenhome/Welcome_Screen.java
+++ app/src/main/java/greenhome/greenhome/Welcome_Screen.java
@@ -1,7 +1,7 @@
 package greenhome.greenhome;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class Welcome_Screen extends AppCompatActivity {
 
