36d50f1dca4e972975d4aac14ec1c663709cbc2a
KashifIV <kashif.h77@gmail.com>
2018-02-17 15:52:14 -0500

    Button for Next Page

---
+++ app/src/main/java/greenhome/greenhome/Question.java
@@ -0,0 +1,28 @@
+package greenhome.greenhome;
+
+import android.app.ActionBar;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by kashi on 2/17/2018.
+ */
+//public enum energyType{Transport, Water, Electricity}
+/*public class Question {
+    private String txt;
+    private energyType qType;
+    public Question(String q, energyType val)
+    {
+        txt = q;
+        qType = val;
+    }
+    public void CreateQuestion()
+    {
+        ImageView banner = new ImageView();
+        banner.setImageResource(R.drawable.questioncard);
+        ViewGroup.LayoutParams
+
+
+    }
+}
+*/
