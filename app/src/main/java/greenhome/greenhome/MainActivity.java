cbe8532d5e263a90e13abcbb9cc71f4fb6a13d3b
Gabriel Tu <gabriel8215@hotmail.com>
2018-02-17 16:20:03 -0500

    Added tabs to the "welcome" screen

--- app/src/main/java/greenhome/greenhome/MainActivity.java
+++ app/src/main/java/greenhome/greenhome/MainActivity.java
@@ -3,6 +3,7 @@ package greenhome.greenhome;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
