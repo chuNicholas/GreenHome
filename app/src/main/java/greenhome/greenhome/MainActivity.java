143225f3858f70246fa0e36059fef04a2e242743
Gabriel Tu <gabriel8215@hotmail.com>
2018-02-17 17:38:59 -0500

    Added 3 fragments for the tabs

--- app/src/main/java/greenhome/greenhome/MainActivity.java
+++ app/src/main/java/greenhome/greenhome/MainActivity.java
@@ -1,11 +1,13 @@
 package greenhome.greenhome;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
