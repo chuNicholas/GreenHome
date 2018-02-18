8b4b62588388e6ce68553b92c8ecad6e240730e5
Nicholas <chu.Nicholas@hotmail.com>
2018-02-18 10:05:40 -0500

    added context to connect to device

--- greenhome.v2/src/main/java/com/example/greenhome_v2/activity_connect_to_device.java
+++ greenhome.v2/src/main/java/com/example/greenhome_v2/activity_connect_to_device.java
@@ -2,6 +2,7 @@ package com.example.greenhome_v2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_connect_to_device extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class activity_connect_to_device extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_to_device);
+
+        TextView text = findViewById(R.id.withText);
+        text.setText("This section allows you to connect your smart devices with the App and sync " +
+                "its settings in order to gain a better estimate of your ecological footprint.");
+
     }
 }
