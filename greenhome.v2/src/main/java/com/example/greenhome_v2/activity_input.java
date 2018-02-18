c71a4959c785a36e6ce227ed9ee56a6e92e583de
Gabriel Tu <gabriel8215@hotmail.com>
2018-02-18 08:56:28 -0500

    Added location code in the mail button

--- greenhome.v2/src/main/java/com/example/greenhome_v2/activity_input.java
+++ greenhome.v2/src/main/java/com/example/greenhome_v2/activity_input.java
@@ -1,5 +1,6 @@
 package com.example.greenhome_v2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,8 +21,15 @@ public class activity_input extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.package.address");
+                if (launchIntent != null) {
+                    startActivity(launchIntent);//null pointer check in case package name was not found
+                }
+
+
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                       // .setAction("Action", null).show();
             }
         });
     }
